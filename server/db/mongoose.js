68f39ae58dad6208f48ef973bd85bf0b97235692
Phanindra <phani1716@gmail.com>
2017-02-27 23:32:27 +0530

    Seperate test and dev env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
-var db={
-  localhost:'mongodb://localhost:27017/TodoApp',
-  mlab:'mongodb://Phani:phani@ds161209.mlab.com:61209/todoapp1'
-};
-mongoose.connect(process.env.PORT?db.mlab:db.localhost);
-
+ var db={
+//   //localhost:'mongodb://localhost:27017/TodoApp',
+   mlab:'mongodb://Phani:phani@ds161209.mlab.com:61209/todoapp1'
+ };
+//mongoose.connect(process.env.PORT?db.mlab:db.localhost);
+mongoose.connect(db.mlab);
 module.exports={mongoose}
