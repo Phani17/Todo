d7b844240e1d559bc9ba763fbda73332d62bee4c
Phanindra <phani1716@gmail.com>
2017-03-02 08:39:14 +0530

    Add POST /users and token method

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 var env=process.env.NODE_ENV || 'development';
-//console.log('---------------------------ENV----------------',env);
+console.log('---------------------------ENV----------------',env);
 if(env==='development'){
   process.env.PORT=3000;
   process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp';
